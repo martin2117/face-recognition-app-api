2db5070913c3f14d757ace4a491c2b210e10a63a
martin2117 <martin2117@azet.sk>
2020-02-23 23:25:44 +0100

    Updating database host

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image.js');
 const postgres = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-contoured-74367',
         user: 'postgres',
         password: '',
         database: 'face-recognition'
