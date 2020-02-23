cc3849b85cc2b8cbe26f9c6094d3ed5870ece3c8
martin2117 <martin2117@azet.sk>
2020-02-23 23:29:08 +0100

    Changing host to connectionString

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image.js');
 const postgres = knex({
     client: 'pg',
     connection: {
-        host: process.env.DATABASE_URL,
+        connectionString: process.env.DATABASE_URL,
         ssl: true
     }
 });
